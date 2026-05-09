f319d1a8e13d5e1e6cc931368e658125e41626e8
ealvar3z <55966724+ealvar3z@users.noreply.github.com>
2026-05-09 00:13:59 -0400

    finally commit this thing to gh

---
+++ README.md
@@ -0,0 +1,90 @@
+## 9fib (de-nixified fork of [majiru's 9front-in-a-box](https://github.com/majiru/9front-in-a-box))
+
+This repo automates setup and local execution of a 9front virtual machine on
+Arch Linux. Virtual machines run under qemu and connect through drawterm.
+
+The current non-Nix workflow is intentionally narrow:
+
+- host OS: Arch / Omarchy
+- guest arch: amd64
+- filesystems: hjfs, cwfs, gefs
+
+It is meant to make trying 9front easy, not to be a general-purpose 9front
+server deployment tool.
+
+## Dependencies
+
+Install the host packages first:
+
+```bash
+sudo pacman -S --needed go qemu-full expect plan9port
+```
+
+`plan9port` provides the `drawterm` binary used to connect to the guest.
+
+## Setup
+
+Prepare a local disk image with:
+
+```bash
+bin/setup-vm
+```
+
+That creates `9front.hjfs.amd64.qcow2` in the current directory.
+
+The base release image is cached under:
+
+```text
+${XDG_CACHE_HOME:-$HOME/.cache}/9fib/releases
+```
+
+The default `hjfs` disk is a qcow2 overlay backed by that cached release image.
+If you delete the cached release file, recreate the VM with `bin/setup-vm`.
+
+Useful variants:
+
+```bash
+bin/setup-vm --fs cwfs
+bin/setup-vm --fs gefs
+bin/setup-vm --latest
+bin/setup-vm --release 11554
+bin/setup-vm --disk ./my-9front.qcow2
+```
+
+## Running
+
+Start the VM and connect with drawterm:
+
+```bash
+bin/run-vm
+```
+
+The default account is `glenda` with password `password`.
+
+For a terminal-only session:
+
+```bash
+bin/run-vm --nogui
+```
+
+To select another filesystem image or drawterm binary:
+
+```bash
+bin/run-vm --fs cwfs
+bin/run-vm --drawterm /usr/bin/drawterm
+```
+
+Extra flags are passed through to the Go launcher in `run/`, so existing flags
+such as `-debug`, `-m`, or `-cpu` can still be used:
+
+```bash
+bin/run-vm --nogui -debug -m 2G
+```
+
+Instructions for using rio are in the [9front FQA](http://fqa.9front.org/fqa8.html).
+
+## Notes
+
+- `arm64` and `386` are not part of the current non-Nix workflow.
+- `--latest` uses the rolling `https://build.9front.org/9front` build stream.
+- The default release pin remains `11554` until it is updated explicitly.
---
+++ run/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	expect "github.com/google/goexpect"
+)
+
+var (
+	ramFlag      = flag.String("m", "4G", "memory for qemu virtual machine")
+	cpuFlag      = flag.String("cpu", "4", "number of cores for the virtual machine")
+	debugFlag    = flag.Bool("debug", false, "enable debug output")
+	archFlag     = flag.String("arch", "amd64", "architecture of vm")
+	fsFlag       = flag.String("fs", "hjfs", "filesystem backing the vm disk")
+	diskFlag     = flag.String("disk", "", "qcow2 vm disk")
+	ubootFlag    = flag.String("uboot", "u-boot.bin", "uboot binary for arm64")
+	qpathFlag    = flag.String("qpath", "", "optional directory containing qemu binaries")
+	drawtermFlag = flag.String("dt", "drawterm", "drawterm binary")
+	noguiFlag    = flag.Bool("nogui", false, "disable the GUI")
+)
+
+func qemuCmd(qcow string) []string {
+	m := map[string][]string{
+		"amd64": {
+			filepath.Join(*qpathFlag, "qemu-system-x86_64"),
+			"-nic",
+			"user,hostfwd=tcp::17019-:17019",
+			"-enable-kvm",
+			"-m",
+			*ramFlag,
+			"-smp",
+			*cpuFlag,
+			"-nographic",
+			"-drive",
+			"media=disk,if=virtio,index=0",
+		},
+		"arm64": {
+			filepath.Join(*qpathFlag, "qemu-system-aarch64"),
+			"-M",
+			"virt-2.12,gic-version=3",
+			"-cpu",
+			"cortex-a72",
+			"-m",
+			*ramFlag,
+			"-smp",
+			*cpuFlag,
+			"-bios",
+			*ubootFlag,
+			"-device",
+			"virtio-blk-pci-non-transitional,drive=disk",
+			"-nic",
+			"user,hostfwd=tcp::17019-:17019,model=virtio-net-pci-non-transitional",
+			"-nographic",
+			"-drive",
+			"if=none,id=disk",
+		},
+		"386": {
+			filepath.Join(*qpathFlag, "qemu-system-x86_64"),
+			"-nic",
+			"user,hostfwd=tcp::17019-:17019",
+			"-enable-kvm",
+			"-m",
+			*ramFlag,
+			"-smp",
+			*cpuFlag,
+			"-nographic",
+			"-drive",
+			"media=disk,if=virtio,index=0",
+		},
+	}
+	r, ok := m[*archFlag]
+	if !ok {
+		log.Fatal("unsupported arch")
+	}
+	r[len(r)-1] = r[len(r)-1] + ",file=" + qcow
+	return r
+}
+
+func main() {
+	flag.Parse()
+
+	var qcow string
+	if *diskFlag == "" {
+		qcow = fmt.Sprintf("9front.%s.%s.qcow2", *fsFlag, *archFlag)
+	} else {
+		qcow = *diskFlag
+	}
+	if _, err := os.Stat(qcow); err != nil {
+		fmt.Fprintf(os.Stderr, "could not find %s\n", qcow)
+		os.Exit(1)
+	}
+
+	cm := strings.Join(qemuCmd(qcow), " ")
+	if *debugFlag {
+		fmt.Println(cm)
+	}
+	exp, _, err := expect.Spawn(cm, -1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer exp.Close()
+
+	if *debugFlag {
+		exp.Options(expect.Tee(os.Stdout))
+	}
+	exp.Expect(regexp.MustCompile("bootargs is"), -1)
+	exp.Send("\n")
+	exp.Expect(regexp.MustCompile("user"), -1)
+	exp.Send("\n")
+	exp.Expect(regexp.MustCompile("%"), -1)
+	exp.Send(`echo 'key proto=dp9ik dom=9front user=glenda !password=password' >/mnt/factotum/ctl` + "\n")
+	exp.Expect(regexp.MustCompile("%"), -1)
+	exp.Send("ip/ipconfig -6 ether /net/ether0\n")
+	exp.Expect(regexp.MustCompile("%"), -1)
+	exp.Send("ip/ipconfig ether /net/ether0\n")
+	exp.Expect(regexp.MustCompile("%"), -1)
+	exp.Send("ip/ipconfig ether /net/ether0 ra6 recvra 1\n")
+	exp.Expect(regexp.MustCompile("%"), -1)
+	exp.Send("aux/listen1 -t 'tcp!*!17019' /rc/bin/service/tcp17019 &\n")
+	exp.Expect(regexp.MustCompile("listen started"), -1)
+
+	exitch := make(chan struct{})
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		exitch <- struct{}{}
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		if *noguiFlag {
+			cmd := exec.Command(*drawtermFlag, "-G", "-r", ".", "-u", "glenda", "-h", "127.0.0.1", "-a", "127.0.0.1", "-c", "service=cpu rc -lI")
+			cmd.Env = append(cmd.Environ(), "PASS=password")
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			cmd := exec.Command(*drawtermFlag, "-u", "glenda", "-h", "127.0.0.1", "-a", "127.0.0.1", "-c", "rc", "-c", "console=() service=terminal rc -l")
+			cmd.Env = append(cmd.Environ(), "PASS=password")
+			cmd.Run()
+
+		}
+		exitch <- struct{}{}
+	}()
+	<-exitch
+	exp.Send("fshalt\n")
+	exp.Expect(regexp.MustCompile("done halting"), -1)
+	exp.Close()
+}
